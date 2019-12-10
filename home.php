5953f621cce299e44decf342cb7ccbdaaff409dc
kunal421 <58681837+kunal421@users.noreply.github.com>
2019-12-10 13:54:41 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,58 @@
+<html>
+<head> <title> login page </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body{
+	
+  background: url(u.jpg);
+  background-repeat: no-repeat;
+   background-size: 100% 100%;
+  
+
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #000033;
+}
+
+li {
+  float: left;
+  font-family:verdana;
+}
+
+li a {
+  display: block;
+  color: #ff471a;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #000033;
+  color:white;
+}
+p{	color : #FB1BC1 ;
+}
+</style>
+</head>
+<body bgcolor=#e6e6e6>
+<ul>
+  <li><img src="goat1.jpg" alt="My Image" height="55px"></li>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="about.php">About</a></li>
+  <li><a href="info.php">Information</a></li>
+  <li><a href="testimonial.php">Testimonials</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li style="float:right"><a  href="Welcome.html" >LOGOUT</a></li>
+  
+</ul>
+
+
+
+
+</body>
+</html>
