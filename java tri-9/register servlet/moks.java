16a32c3045bbb2ed6fca0ad00bf20a56d5217a84
kunal421 <58681837+kunal421@users.noreply.github.com>
2020-03-11 13:44:57 +0530

    Add files via upload

---
+++ java tri-9/register servlet/moks.java
@@ -0,0 +1,34 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+public class moks extends GenericServlet
+{
+	public void service(ServletRequest req,ServletResponse res) throws IOException, ServletException
+	{
+		PrintWriter out=res.getWriter();
+		String rid=req.getParameter("rid");
+		String regdate=req.getParameter("regdate");
+		String sname=req.getParameter("sname");
+		String mobno=req.getParameter("mobno");
+		String evname=req.getParameter("evname");
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3308/registration","root","1111");
+			PreparedStatement st=con.prepareStatement("insert into re values(?,?,?,?,?)");
+			st.setInt(1,Integer.parseInt(rid));
+			st.setInt(2,Integer.parseInt(regdate));
+			st.setString(3,sname);
+			st.setDouble(4,Double.parseDouble(mobno));
+			st.setString(5,evname);
+			int r=st.executeUpdate();
+			if(r>0)
+				out.println("Registration successfull");
+			else
+				out.println("Try Again");
+		}
+		catch(Exception e)
+		{	out.println(e);	}	
+	}
+}
---
+++ java tri-9/register servlet/mypath.bat
@@ -0,0 +1,2 @@
+set path=C:\Program Files\Java\jdk1.8.0_191\bin;
+set classpath=D:\mysql-connector-java-5.1.48.jar;D:\Tomcat\lib\servlet-api.jar;C:\Program Files\Java\jdk1.8.0_191\lib;
